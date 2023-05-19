3dbfc739ee4eba79e0f0c627fce8f31502ba48a5
jgange <jgange@users.noreply.github.com>
2023-05-18 19:18:12 -0500

    added system configuration support

--- Program.cs
+++ Program.cs
@@ -6,36 +6,32 @@ class Program
 {
     static void Main()
     {
-        AttributeList attributes = new AttributeList(
-            new Attribute("Strength",10),
-            new Attribute("Dexterity",10),
-            new Attribute("Constitution",10),
-            new Attribute("Intellect",10),
-            new Attribute("Power",10),
-            new Attribute("Size",10)
-        );
+        AttributeList? attributesFromDisk = new AttributeList();
 
         Race characterRace = new Race();
         characterRace.RaceName = "Human";
         characterRace.RaceType = "Base human";
-        characterRace.baseAttributes = attributes;
+        string fileName = ReadSetting("fileName");
+        //characterRace.baseAttributes = 
+        AttributeList.ReadFromDisk(fileName, out attributesFromDisk);
+        
 
         characterRace.DisplayRace();
-
-        ReadSetting("fileName");  
     }
 
-    static void ReadSetting(string key)  
+    static string ReadSetting(string key)  
     {  
+        string result = "";
         try  
         {  
             var appSettings = ConfigurationManager.AppSettings;  
-            string result = appSettings[key] ?? "Not Found";  
-            Console.WriteLine(result);  
+            result = appSettings[key] ?? "Not Found";
+            return result;
         }  
         catch (ConfigurationErrorsException)  
         {  
-            Console.WriteLine("Error reading app settings");  
+            Console.WriteLine("Error reading app settings");
+            return result;
         }  
     }  
 }
